6bd603556efe2fb2b402c3b72f513d1bd17d7398
Kyle <Jefforyheath@mail.weber.edu>
2022-12-28 00:47:38 -0700

    Added information for inheritance in the ARTDEnemies class

--- AlternateRealityTheDungeon/src/AlternateRealityTheDungeon/ARTDEnemies.java
+++ AlternateRealityTheDungeon/src/AlternateRealityTheDungeon/ARTDEnemies.java
@@ -2,6 +2,8 @@ package AlternateRealityTheDungeon;
 
 //https://gamedev.stackexchange.com/questions/81884/making-a-monster-encounter-method
 
+//https://www.geeksforgeeks.org/inheritance-in-java/
+
 public class ARTDEnemies 
 {
 
