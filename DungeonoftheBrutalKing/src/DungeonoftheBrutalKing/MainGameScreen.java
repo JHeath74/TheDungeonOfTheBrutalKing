1e4a1c3a895029bead4e9d654826a2b53de56a50
JHeath74 <33070831+JHeath74@users.noreply.github.com>
2024-04-18 14:47:47 -0600

    Update MainGameScreen.java

--- DungeonoftheBrutalKing/src/DungeonoftheBrutalKing/MainGameScreen.java
+++ DungeonoftheBrutalKing/src/DungeonoftheBrutalKing/MainGameScreen.java
@@ -54,6 +54,8 @@ import GameEngine.Texture;
 public class MainGameScreen extends JFrame implements Runnable {
 
 //Do a callable or a runable?
+//https://howtodoinjava.com/java/multi-threading/java-runnable-vs-thread/
+
 	private static final long serialVersionUID = 1L;
 	Charecter myChar = Charecter.Singleton();
 	GameSettings myGameSettings = new GameSettings();
